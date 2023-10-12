78d00854f8a069ca4423184b2a4a4e0a940b5e47
myakop <myakop13@gmail.com>
2023-10-12 13:55:29 +0300

    add search method

--- src/application/TrackingCustomers.java
+++ src/application/TrackingCustomers.java
@@ -2,9 +2,11 @@ package application;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Collection;
 import java.util.ResourceBundle;
 
-
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,6 +16,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
@@ -22,6 +25,9 @@ public class TrackingCustomers implements Initializable {
 	
 	@FXML
 	private ListView<Customer> customers;
+	
+	 @FXML
+	    private TextField search;
 	
 	
 
@@ -32,8 +38,27 @@ public class TrackingCustomers implements Initializable {
 		
 		customers.getItems().addAll(Main.gym.getCustomers().values());
 		
+		search.textProperty().addListener((observable, oldValue, newValue) -> {
+	        filterCustomers(newValue);
+	    });
+		
 	}
-	
+	private void filterCustomers(String keyword) {
+	    Collection<Customer> allCustomers = Main.gym.getCustomers().values();
+	    
+	    // Convert the collection to an ObservableList
+	    ObservableList<Customer> customerList = FXCollections.observableArrayList(allCustomers);
+
+	    // Create a filtered list based on the keyword
+	    ObservableList<Customer> filteredCustomers = customerList.filtered(customer -> {
+	        // You can use different conditions to filter by customer name, e.g., using contains() for a case-insensitive search
+	        return customer.getFirstName().toLowerCase().contains(keyword.toLowerCase());
+	    });
+
+	    // Update the ListView to show the filtered results
+	    customers.setItems(filteredCustomers);
+	}
+
 	
 	 
 		 
